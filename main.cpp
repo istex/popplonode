5588eec1464c63785d601fa3f0718ddc390443c2
Rémy MEJA <remy.meja@inist.fr>
2017-08-23 14:51:19 +0200

    adding GetTextFromPage function

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <memory>
 #include <nan.h>
 #include <string>
+#include <iostream>
 
 using namespace std;
 using namespace v8;
@@ -17,6 +18,7 @@ public:
 
     Nan::SetPrototypeMethod(tpl, "load", load);
     Nan::SetPrototypeMethod(tpl, "getMetadata", getMetadata);
+    Nan::SetPrototypeMethod(tpl, "getTextFromPage", getTextFromPage);
 
     constructor().Reset(Nan::GetFunction(tpl).ToLocalChecked());
     Nan::Set(target, Nan::New("Popplonode").ToLocalChecked(), Nan::GetFunction(tpl).ToLocalChecked());
@@ -57,12 +59,30 @@ private:
     Popplonode* popplonode = Nan::ObjectWrap::Unwrap<Popplonode>(info.Holder());
     Local<Object> metadata = Nan::New<Object>();
     vector<string> infoKeys{popplonode->doc->info_keys()};
-    for_each(infoKeys.begin(), infoKeys.end(), [&](string infoKey) { 
+    for_each(infoKeys.begin(), infoKeys.end(), [&](string infoKey) {
       Local<String> infoProp = Nan::New<String>(infoKey).ToLocalChecked();
       Local<String> infoValue = Nan::New<String>(popplonode->doc->info_key(infoKey).to_latin1().c_str()).ToLocalChecked();
       metadata->Set(infoProp, infoValue);
     });
     info.GetReturnValue().Set(metadata);
+  }
+
+  static NAN_METHOD(getTextFromPage) {
+    if (info.Length() < 2)
+      return Nan::ThrowTypeError("Load requires at least 2 argument");
+    double pageNumber = info[0]->NumberValue();
+    Nan::Callback *callback = new Nan::Callback(info[1].As<Function>());
+    Popplonode* popplonode = Nan::ObjectWrap::Unwrap<Popplonode>(info.Holder());
+    poppler::page* currentPage{popplonode->doc->create_page(pageNumber)};
+    if (currentPage != nullptr) {
+      auto byteArrayResult = currentPage->text().to_utf8();
+      string result{byteArrayResult.begin(), byteArrayResult.end()};
+      Local<Value> argv[] = {Nan::Null(), Nan::New<String>(result.c_str()).ToLocalChecked()};
+      callback->Call(2, argv);
+    } else {
+      Local<Value> argv[] = {Nan::Error("Page corrupted")};
+      callback->Call(1, argv);
+    }
   }
 
   static inline Nan::Persistent<v8::Function>& constructor() {
--- test/simple-test.js
+++ test/simple-test.js
@@ -2,3 +2,7 @@ const Popplonode = require('../index.js');
 const pdf = new Popplonode();
 console.log(pdf.load('data/test.pdf'));
 console.log(pdf.getMetadata());
+pdf.getTextFromPage(0, (error, text) => {
+  if (error) return console.log(error);
+  console.log(text);
+});
