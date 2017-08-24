84afcfab7539fb862b2b2e11d1de846e7a9d0ffb
Rémy MEJA <remy.meja@inist.fr>
2017-08-24 14:55:31 +0200

    adding proper unit tests

---
+++ test/popplonode-test.js
@@ -0,0 +1,45 @@
+const Popplonode = require('../index.js');
+const expect = require('chai').expect;
+
+describe('popplonode', function () {
+  it('should construct an new Popplonode object', function () {
+    const poppl = new Popplonode();
+    expect(poppl).to.be.instanceOf(Popplonode);
+  });
+
+  it('should load a pdf file', function () {
+    const poppl = new Popplonode();
+    expect(poppl.load('test/data/test.pdf')).to.true;
+  });
+
+  it('should get the metadata from pdf file', function () {
+    const poppl = new Popplonode();
+    poppl.load('test/data/test.pdf');
+    const metadata = poppl.getMetadata();
+    expect(metadata).to.be.an('object');
+    expect(metadata).to.have.own.property('CreationDate');
+    expect(metadata.CreationDate).to.be.a('string');
+    expect(metadata).to.have.own.property('Author');
+    expect(metadata.Author).to.be.a('string');
+    expect(metadata).to.have.own.property('Creator');
+    expect(metadata.Creator).to.be.a('string');
+    expect(metadata).to.have.own.property('Producer');
+    expect(metadata.Producer).to.be.a('string');
+    expect(metadata).to.have.own.property('ModDate');
+    expect(metadata.ModDate).to.be.a('string');
+    expect(metadata).to.have.own.property('Title');
+    expect(metadata.Title).to.be.a('string');
+    expect(metadata).to.have.own.property('TotalNbPages');
+    expect(metadata.TotalNbPages).to.be.a('number');
+  });
+
+  it('should get the text from page of pdf file', function (done) {
+    const poppl = new Popplonode();
+    poppl.load('test/data/test.pdf');
+    poppl.getTextFromPage(0, (error, content) => {
+      if (error) return done(error);
+      expect(content).to.be.a('string');
+      done();
+    });
+  });
+});
--- test/simple-test.js
+++
@@ -1,8 +0,0 @@
-const Popplonode = require('../index.js');
-const pdf = new Popplonode();
-console.log(pdf.load('data/test.pdf'));
-console.log(pdf.getMetadata());
-pdf.getTextFromPage(0, (error, text) => {
-  if (error) return console.log('error à la dude: ', error);
-  console.log('text :', text);
-});
