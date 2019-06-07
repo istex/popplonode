d6ed43540ccf62f431cc3585279037ad2a5a97f6
Rémy MEJA <remymeja@free.fr>
2019-06-07 23:26:58 +0200

    fix function signature

--- popplonode.cpp
+++ popplonode.cpp
@@ -44,7 +44,7 @@ NAN_METHOD(Popplonode::New) {
     const int argc = 1;
     Local<Value> argv[argc] = {info[0]};
     Local<Function> cons = Nan::New(constructor());
-    info.GetReturnValue().Set(cons->NewInstance(argc, argv));
+    info.GetReturnValue().Set(Nan::NewInstance(cons, argc, argv).ToLocalChecked());
   }
 }
 NAN_METHOD(Popplonode::load) {
